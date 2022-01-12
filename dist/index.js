cb87e8de525eb6dbbd1015ffcf402aa4fc06d04d
Kael Shipman <kael.shipman@gmail.com>
2022-01-11 19:13:01 -0600

    Releasing RC1

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-//import * as fs from "fs";
-console.log(process.argv);
-console.log(process.env);
-//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,195 @@
-//import * as fs from "fs";
+import { promises as fs } from "fs";
 
-console.log(process.argv);
-console.log(process.env);
+/**
+ * Return helptext
+ */
+async function helptext(repopath: string): Promise<string> {
+  const types = await getTypes(repopath);
+  return `@wymp/boilerplate - Create a new project based on pre-defined boilerplate from Wymp
+
+SYNOPSIS
+    npx @wymp/boilerplate create [type] [target]
+    npx @wymp/boilerplate --version
+    npx @wymp/boilerplate --help|-h
+
+DESCRIPTION
+    This script is intended to be used via npx to create a new project based on pre-defined
+    boilerplate.  Target may be any directory, existing or new. If the directory already exists,
+    boilerplate files are simply copied to it. If the directory does not yet exist, it is created
+    and then boilerplate files are copied to it.
+
+TYPES
+    Available types of boilerplate are the following:
+
+      * ${types.join("\n      * ")}
+
+`;
+}
+
+/**
+ * Return version information
+ */
+async function version(repopath: string): Promise<string> {
+  const pkgstr = await fs.readFile(`${repopath}/package.json`, "utf8");
+  const pkg = <any>JSON.parse(pkgstr);
+  return `@wymp/boilerplate v${pkg.version}`;
+}
+
+/**
+ * Return an array of available types of boilerplate, based on subdirectories under `/templates`
+ */
+async function getTypes(repopath: string): Promise<Array<string>> {
+  const boilerplates = await fs.readdir(`${repopath}/templates`);
+  return boilerplates.map(path => path.split(/[\/\\]/).pop()!);
+}
+
+/**
+ * Shortcut to check to see if a file or directory exists
+ */
+function exists(path: string): Promise<boolean> {
+  return fs.access(path).then(_ => true).catch(e => false);
+}
+
+/**
+ * Copy everything from `src` into `targ`, first ensuring that `targ` exists.
+ * @returns An array (possible empty) of error messages.
+ */
+async function copyAll(src: string, targ: string): Promise<Array<string>> {
+  // Create the target, if necessary
+  await fs.mkdir(targ, { recursive: true });
+
+  // Now recurisvely copy everything from src into targ
+  const files = await fs.readdir(src, { encoding: "utf8", withFileTypes: true });
+  const p: Array<Promise<Array<string>>> = [];
+  for (const file of files) {
+    if (file.isDirectory()) {
+      p.push(
+        copyAll(`${src}/${file.name}`, `${targ}/${file.name}`)
+        .catch(e => [e.message])
+      );
+    } else {
+      p.push(
+        fs.copyFile(`${src}/${file.name}`, `${targ}/${file.name}`)
+        .then(_ => <Array<string>>[])
+        .catch(e => [e.message])
+      );
+    }
+  }
+  const errorSets = await Promise.all(p);
+  return errorSets.reduce<Array<string>>((agg, cur) => agg.concat(cur), []);
+}
+
+/**
+ * Validate inputs and execute the requested command
+ */
+async function go(argv: Array<string>) {
+  // First make sure we can locate our repo path, since all other functionality depends on that
+  const binary = argv[1];
+  const pathParts = binary.split(/[\/\\]/);
+  let repopath: string | false = `${pathParts.slice(0, pathParts.length - 1).join("/")}`;
+
+  // Analyze the directory tree to see if we can find the repo path
+  let n = 1;
+  while (true) {
+    // If we've tried more than three times, abort
+    if (n >= 3) {
+      repopath = false;
+      break;
+    }
+
+    // Check to see if we've got both templates and package.json
+    const [ pkgExists, templatesExists ] = await Promise.all([
+      exists(`${repopath}/package.json`),
+      exists(`${repopath}/templates`),
+    ]);
+    if (pkgExists && templatesExists) {
+      // if so, we found the repopath! Break out
+      break;
+    } else {
+      // Otherwise, if lib exists, go forward
+      const libExists = await exists(`${repopath}/lib`);
+      if (libExists) {
+        repopath = `${repopath}/lib`;
+      } else {
+        // Otherwise, go back another directory
+        const pathParts: Array<string> = repopath.split(/[\/\\]/);
+        repopath = `${pathParts.slice(0, pathParts.length - 1).join("/")}`;
+      }
+    }
+
+    // Augment our attempt counter
+    n++;
+  }
+
+  // If we couldn't find it, throw an error
+  if (!repopath) {
+    throw new Error(`Couldn't find the @wymp/boilerplate repo directory! Can't proceed :(`);
+  }
+
+  // Now that we have a repo directory, service the command
+  const cmd = argv[2];
+
+  // Version
+  if (cmd === "--version") {
+    console.log(await version(repopath));
+    return;
+  }
+
+  // Help
+  if (cmd === "--help" || cmd === "-h") {
+    console.log(await helptext(repopath));
+    return;
+  }
+
+  // Create
+  if (cmd === "create") {
+    // Make sure we've got a valid type and target
+    const typ = argv[3];
+    if (!typ) {
+      throw new Error(
+        `You must specify a type of boilerplate to use and a target directory. See ` +
+        `'@wymp/boilerplate --help' for help.`
+      );
+    }
+    const validTypes = await getTypes(repopath);
+    if (!validTypes.find(t => t === typ)) {
+      throw new Error(
+        `The type of boilerplate you've specified, '${typ}', is not one of the available types ` +
+        `offered. Please see '@wymp/boilerplate --help' for the available types of boilerplate.`
+      );
+    }
+
+    const _targ = argv[4];
+    if (!_targ) {
+      throw new Error(
+        `You must specify a target directory to create your project in. See '@wymp/boilerplate ` +
+        `--help' for help.`
+      );
+    }
+    const targ = _targ[_targ.length-1] === "/" ? _targ.substring(0, _targ.length - 1) : _targ;
+
+    // Copy everything from boilerplate into targ
+    const errors = await copyAll(`${repopath}/templates/${typ}`, targ);
+
+    if (errors.length > 0) {
+      throw new Error(
+        `Copy failed. There were ${errors.length} errors when copying files:\n\n` +
+        `  * ${errors.join("\n  * ")}`
+      );
+    }
+
+    // Return
+    return;
+  }
+
+  // Anything else
+  throw new Error(
+    `Unknown command or option '${cmd}'. Try 'npx @wymp/boilerplate --help' for options.`
+  );
+}
+
+// Execute.
+go(process.argv)
+  .catch(e => {
+    console.error(`Error: ${e.message}`);
+  });
